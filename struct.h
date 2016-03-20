1b3e547a25d57b5dae343d784be2d090a6d551c6
raitei3 <antoine33.borde@gmail.com>
2016-03-20 22:41:09 +0100

    début de lex et struct.h

---
+++ struct.h
@@ -0,0 +1,23 @@
+
+#include <stdbool.h>
+
+struct tree;
+struct attributes;
+enum type {tree, word};        //typage des nœuds: permet de savoir si un nœud construit
+                               //un arbre ou s'il s'agit simplement de texte
+
+struct attributes{
+    char * key;               //nom de l'attribut
+    char * value;             //valeur de l'attribut
+    struct attributes * next; //attribut suivant
+};
+
+struct tree {
+    char * label;              //étiquette du nœud
+    bool nullary;              //nœud vide, par exemple <br/>
+    bool space;                //nœud suivi d'un espace
+    enum type tp;              //type du nœud. nullary doit être true s tp vaut word
+    struct attributes * attr;  //attributs du nœud
+    struct tree * daughters;   //fils gauche, qui doit être NULL si nullary est true
+    struct tree * right;       //frère droit
+};
