2b89edb2b01b41063a5a8478206352b9152d8fd1
marbazalgette <marin.bazalgette@gmail.com>
2016-03-21 18:24:33 +0100

    attributes.c

--- attributes.c
+++ attributes.c
@@ -5,9 +5,33 @@
 
 attributes attributes_create(char * key, char * value)
 {
-  attributes a = malloc(sizeof(attributes));
-  a->key = key;
-  a->value = value;
-  a->next = NULL;
-  return a;
+  attributes attr = malloc(sizeof(attributes));
+  attr->key = key;
+  attr->value = value;
+  attr->next = NULL;
+  return attr;
+}
+
+char * get_attribute_key (attributes attr) {
+  return attr->key;
+}
+ 
+void set_attribute_key (attributes attr, char * key) {
+  attr->key = key;
+}
+
+char * get_attribute_value (attributes attr) {
+  return attr->value;
+}
+
+void set_attribute_value (attributes attr, char * value) {
+  attr->value = value;
+}
+
+attributes get_attribute_next (attributes attr) {
+  return attr->next;
+}
+
+void set_attribute_next (attributes attr, attributes next) {
+  attr->next = next;
 }
