8125a9895313a9132d57ca3dcc7774a021775e10
marbazalgette <marin.bazalgette@gmail.com>
2016-03-21 18:18:00 +0100

    oops

--- tree.c
+++ tree.c
@@ -76,37 +76,6 @@ void set_enum(tree t,enum type tp){
 }
 
 
-attributes attributes_create (char * key, char * value, struct attributes_t * next) {
-  attributes attr = malloc(sizeof(attributes));
-  attr->key=key;
-  attr->value=value;
-  attr->next = next;
-  return attr;
-}
-
-char * get_attribute_key (attributes attr) {
-  return attr->key;
-}
- 
-void set_attribute_key (attributes attr, char * key) {
-  attr->key = key;
-}
-
-char * get_attribute_value (attributes attr) {
-  return attr->value;
-}
-
-void set_attribute_value (attributes attr, char * value) {
-  attr->value = value;
-}
-
-attributes get_attribute_next (attributes attr) {
-  return attr->next;
-}
-
-void set_attribute_next (attributes attr, attributes next) {
-  attr->next = next;
-}
 
 
 
