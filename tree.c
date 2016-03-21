8350b426d392ac3eb384585d1ce04d151188c675
raitei3 <antoine33.borde@gmail.com>
2016-03-21 17:11:29 +0100

    accesseur de tree

--- tree.c
+++ tree.c
@@ -17,3 +17,60 @@ tree tree_create(char * label, bool nullary, bool space, enum type tp,  attribut
   return t;
 }
 
+tree get_daughters(tree t){
+  return t->daughters;
+}
+
+void set_daughters (tree t,tree d){
+  t->daughters = d;
+}
+
+tree get_right(tree t){
+  return t->right;
+}
+
+void set_right (tree t,tree r){
+  t->right = r;
+}
+
+char* get_label(tree t){
+  return t->label;
+}
+
+void set_label (tree t,char * label){
+  t->label = label;
+}
+
+attributes get_attributes(tree t){
+  return t->attr;
+}
+
+void set_attributes (tree t,attributes attr){
+  t->attr = attr;
+}
+
+bool get_nullary(tree t){
+  return t->nullary;
+}
+
+void set_nullary (tree t,bool nullary){
+  t->nullary = nullary;
+}
+
+bool get_space(tree t){
+  return t->space;
+}
+
+void set_space (tree t,bool space){
+  t->space = space;
+}
+
+enum type get_enum(tree t){
+  return t->tp;
+}
+
+void set_enum(tree t,enum type tp){
+  t->tp = tp;
+  if (tp == WORD)
+    t->nullary = true;
+}
