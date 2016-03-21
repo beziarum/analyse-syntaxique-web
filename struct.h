c709e9fb9c4ac32ff5b13face1cc2e7419d6b500
raitei3 <antoine33.borde@gmail.com>
2016-03-21 01:01:18 +0100

    modification de struct.h

--- struct.h
+++ struct.h
@@ -1,23 +1,27 @@
 
 #include <stdbool.h>
 
-struct tree;
-struct attributes;
-enum type {tree, word};        //typage des nœuds: permet de savoir si un nœud construit
+struct tree_t;
+struct attributes_t;
+
+enum type {TREE,WORD};        //typage des nœuds: permet de savoir si un nœud construit
                                //un arbre ou s'il s'agit simplement de texte
 
-struct attributes{
+struct attributes_t{
     char * key;               //nom de l'attribut
     char * value;             //valeur de l'attribut
-    struct attributes * next; //attribut suivant
+    struct attributes_t * next; //attribut suivant
 };
 
-struct tree {
+struct tree_t {
     char * label;              //étiquette du nœud
     bool nullary;              //nœud vide, par exemple <br/>
     bool space;                //nœud suivi d'un espace
-    enum type tp;              //type du nœud. nullary doit être true s tp vaut word
-    struct attributes * attr;  //attributs du nœud
-    struct tree * daughters;   //fils gauche, qui doit être NULL si nullary est true
-    struct tree * right;       //frère droit
+    enum type tp;              //type du nœud. nullary doit être true si tp vaut word
+    struct attributes_t * attr;  //attributs du nœud
+    struct tree_t * daughters;   //fils gauche, qui doit être NULL si nullary est true
+    struct tree_t * right;       //frère droit
 };
+
+typedef struct tree_t * tree;
+typedef struct attributes_t * attributes;
