c6c7efa9ae2156f383da8b4d19dad275054cec7c
raitei3 <antoine33.borde@gmail.com>
2016-03-21 01:00:15 +0100

    debut de tree.c

---
+++ tree.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+tree tree_create(char * label, bool nullary, bool space, enum type tp,  attributes attr, tree daughters, tree right){
+  if ((tp == WORD) && (nullary == false))
+    return NULL;
+  tree t = malloc(sizeof (tree));
+  t->label = label;
+  t->nullary = nullary;
+  t->space = space;
+  t->tp = tp;
+  t->attr = attr;
+  t->daughters = daughters;
+  t->right = right;
+  return t;
+}
+
+
+
+void main(){};
