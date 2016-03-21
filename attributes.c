6268c1c2dc102f110f131ced6e55f59bca4d0f78
marbazalgette <marin.bazalgette@gmail.com>
2016-03-21 18:20:56 +0100

    attribute.c

--- attributes.c
+++ attributes.c
@@ -11,3 +11,27 @@ attributes attributes_create(char * key, char * value)
   a->next = NULL;
   return a;
 }
+
+char * get_attribute_key (attributes attr) {
+  return attr->key;
+}
+ 
+void set_attribute_key (attributes attr, char * key) {
+  attr->key = key;
+}
+
+char * get_attribute_value (attributes attr) {
+  return attr->value;
+}
+
+void set_attribute_value (attributes attr, char * value) {
+  attr->value = value;
+}
+
+attributes get_attribute_next (attributes attr) {
+  return attr->next;
+}
+
+void set_attribute_next (attributes attr, attributes next) {
+  attr->next = next;
+}
