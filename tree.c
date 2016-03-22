ffc29171967a91513810b10b514e9ee6b6dfd38a
raitei3 <antoine33.borde@gmail.com>
2016-03-22 15:40:36 +0100

    nouvelle fonction display + correction erreur attributes_create + ajout des prototype de attribute dans struct.h + module de test pour display_tree dans le main

--- tree.c
+++ tree.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include "struct.h"
 
+void display_tree_rec(tree t,int p);
+
 tree tree_create(char * label, bool nullary, bool space, enum type tp,  attributes attr, tree daughters, tree right){
   if ((tp == WORD) && (nullary == false))
     return NULL;
-  tree t = malloc(sizeof (tree));
+  tree t = malloc(sizeof (struct tree_t));
   t->label = label;
   t->nullary = nullary;
   t->space = space;
@@ -75,7 +77,7 @@ void set_enum(tree t,enum type tp){
     t->nullary = true;
 }
 
-void display_tree(tree t,int fils,int frere){
+/*void display_tree(tree t,int fils,int frere){
   if((fils == 0) && (frere==0))
     printf("racine de l'arbre ");
   else 
@@ -83,4 +85,19 @@ void display_tree(tree t,int fils,int frere){
   printf("label = %s \n",t->label);
   display_tree(t->daughters,fils+1,frere);
   display_tree(t->right,fils,frere+1);
+  }*/
+
+
+void display_tree(tree t){
+  display_tree_rec(t,1);
+}
+
+void display_tree_rec(tree t,int p){
+  if(t == NULL)
+    return;
+  for(int i =0; i<p;i++)
+    printf("|");
+  printf(" %s\n",t->label);
+  display_tree_rec(t->daughters,p+1);
+  display_tree_rec(t->right,p);
 }
